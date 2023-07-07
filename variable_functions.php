59c296bd22a632e237366943911d64ad30de242e
Valithor Obsidion <valzargaming@gmail.com>
2023-07-07 09:26:30 -0400

    Debugging

--- variable_functions.php
+++ variable_functions.php
@@ -146,12 +146,12 @@ $manager_message = function (\Tutelar\Tutelar $tutelar, $message, string $messag
                 if (!$emoji) return $message->reply("Missing emoji parameter! Creating new custom roles should be done in the format of @{$tutelar->discord->username} add role_name unicode_emoji");
                 $message->react($emoji)->done(
                     function ($reaction) use ($tutelar, $message, $name, $emoji) { //Unicode should be valid, so create the role
-                        $index = isset($tutelar->discord_config[$message->guild_id]['reaction_roles']['custom']['roles']) ? sizeof($tutelar->discord_config[$message->guild_id]['reaction_roles']['custom']['roles']) : 0;
+                        $tutelar->logger->debug('[INDEX]', $index = isset($tutelar->discord_config[$message->guild_id]['reaction_roles']['custom']['roles']) ? sizeof($tutelar->discord_config[$message->guild_id]['reaction_roles']['custom']['roles']) : 0);;
                         $increment = '';
                         if ($index >= 20) {
                             do {
                                 $increment++;
-                                $newIndex = isset($tutelar->discord_config[$message->guild_id]['reaction_roles']["custom$increment"]['roles']) ? sizeof($tutelar->discord_config[$message->guild_id]['reaction_roles']["custom$increment"]['roles']) : 0;
+                                $tutelar->logger->debug('[NEW INDEX]', $newIndex = isset($tutelar->discord_config[$message->guild_id]['reaction_roles']["custom$increment"]['roles']) ? sizeof($tutelar->discord_config[$message->guild_id]['reaction_roles']["custom$increment"]['roles']) : 0);
                             } while ($newIndex >= 20);
                             if ($newIndex >= 20) {
                                 $increment++;
